090c961df464546657d4728293d1f4f378fc21f3
Alexander Hill <32838443+Alex-Hill94@users.noreply.github.com>
2022-06-07 16:47:59 +0100

    Create computation.py

---
+++ Examples/computation.py
@@ -0,0 +1,33 @@
+import numpy as np
+import h5py as h5
+from decimal import *
+from numpy import matrix
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from pylab import *
+from Hydrogen_Phases import Compute_H_Fractions
+
+path = 'test_data.hdf5'
+u = h5.File(path, 'r')
+
+PIDs = u['PIDs'][()]
+Density = u['Density'][()]
+Mass = u['Mass'][()]
+Temperature = u['Temperature'][()]
+H_abundance = u['H_abundance'][()]
+Redshift = u['Redshift'][()]
+SFR = u['SFR'][()]
+u.close()
+
+
+C = Compute_H_Fractions(Redshift = Redshift,
+            H_abun = H_abundance,
+            Temp = Temperature,
+            Density = Density,
+            SFR = SFR,
+            Mass = Mass,
+            PIDs = PIDs)
+
+C.compute()
+C.plot_example()
